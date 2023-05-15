377eae68b3e61e8647af6b6770d4b38bd7016800
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-15 14:24:23 -0700

    added function 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,15 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (tree->parent == NULL)
+	if (!tree->parent)
 	{
 		if (left >= right)
 			return (left);
-		else
-			return (right);
+		return (right);
 	}
 	if (left >= right)
 		return (left + 1);
-	else
-		return (right + 1);
+	return (right + 1);
 }
