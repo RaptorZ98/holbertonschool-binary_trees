4f19f1e4452658c74e374a4af21d54287d16837d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-15 15:19:17 -0700

    added function 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,32 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to measure
+ * Return: the height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (!tree->parent)
+	{
+		if (left >= right)
+			return (left);
+		return (right);
+	}
+	if (left >= right)
+		return (left + 1);
+	return (right + 1);
 }
