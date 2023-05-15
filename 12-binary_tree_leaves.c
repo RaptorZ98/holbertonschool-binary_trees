1d6f49df87bbf5cb64f39d0bcb8f05994b940a43
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-15 10:56:46 -0700

    added function 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: the tree to count the leaves from
+ * Return: the number of leafs in the tree
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
