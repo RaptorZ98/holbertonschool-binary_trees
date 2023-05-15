5f6fb2d564413049cb3fd066302616e81c01519a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-15 15:11:43 -0700

    corrected tree hight

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (binary_tree_is_full(tree) == 1
-	&& (binary_tree_height(tree) == binary_tree_height(tree)))
+	&& (binary_tree_height(tree->left) == binary_tree_height(tree->right)))
 		return (1);
 	return (0);
 }
