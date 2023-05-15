a3abc26a06ba1ed85bf94dc71b480978800e5820
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-15 11:02:50 -0700

    added function 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count the nodes that have at least one child
+ * @tree: the tree to count the nodes from
+ * Return: the number of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+		return (1);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
